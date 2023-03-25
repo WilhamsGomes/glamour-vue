30c508ddb90e42be98283a23a7b8082730cf676d
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2023-03-25 13:34:11 -0300

    Editando eventos

--- src/components/CarouselInfo.vue
+++ src/components/CarouselInfo.vue
@@ -1,61 +1,16 @@
 <template>
     <div class="events">
       <div class="container">
-        <div class="card" @mouseover="preEvento = true" @mouseleave="preEvento = false">
-            <h3 class="title">PRÉ EVENTO</h3>
+        <div class="card" v-for="(item, index) in items" :key="index" >
+            <h3 class="title">{{ item.title }}</h3>
+            <img src="https://www.pucgoias.edu.br/wp-content/uploads/2022/02/DSC_6379-1.jpg" alt="evento"/>
             <div class="bar">
-                <div class="emptybar">
-                    <p>Pré evento é o primeiro evento oficial da turma. Realização de ensaios fotográficos para confecção dos materiais gráficos.</p>
-                </div>
+                <div class="emptybar"> </div>
                 <div class="filledbar"></div>
             </div>
-        </div>
-        <div class="card" @mouseover="aulaSaudade = true" @mouseleave="aulaSaudade = false">
-            <h3 class="title">AULA DA <br> SAUDADE</h3>
-            <div class="bar">
-                <div class="emptybar">Concretiza-se a finalização de um ciclo. Momento no qual a turma recorda todos os milênios vividos juntos na instituição.</div>
-                <div class="filledbar"></div>
-            </div>
-        </div>
-        <div class="card" @mouseover="cultoEcumenico = true" @mouseleave="cultoEcumenico = false">
-            <h3 class="title">CULTO ECUMÊNICO</h3>
-            <div class="bar">
-                <div class="emptybar">Cerimônia religiosa de formatura em que as pessoas refletem, agradecem e se conectam com sua fé.</div>
-                <div class="filledbar"></div>
-            </div>
-        </div>
-        <div class="card" @mouseover="colacaoDeGrau = true" @mouseleave="colacaoDeGrau = false">
-            <h3 class="title">COLAÇÃO DE GRAU</h3>
-            <div class="bar">
-                <div class="emptybar">Cerimônia acadêmica tradicional e de caráter obrigatório para a outorga de grau do bacharel ou licenciado.</div>
-                <div class="filledbar"></div>
-            </div>
-        </div>
-        <div class="card" @mouseover="baileDeFormatura = true" @mouseleave="baileDeFormatura = false">
-            <h3 class="title">BAILE DE FORMATURA</h3>
-            <div class="bar">
-                <div class="emptybar">O pré-evento é o momento em que a organização é efetivamente feita.</div>
-                <div class="filledbar"></div>
-            </div>
-        </div>
-        <div class="card" @mouseover="eventosCorporativos = true" @mouseleave="eventosCorporativos = false"> 
-            <h3 class="title">EVENTOS CORPORATIVOS</h3>
-            <div class="bar">
-                <div class="emptybar">São uma das vias que nos proporcionam experiências e interações. Feiras, Workshops, confraternizações...</div>
-                <div class="filledbar"></div>
-            </div>
+            <p>{{ item.description }}</p>
         </div>
       </div> 
-      <div class="galeria" >
-        <div class="imagem-div" v-for="item, index in 4" :key="index">
-          <div class="img-preEvento" v-if="preEvento">IMAGEM PRÉ-EVENTO</div>
-          <div class="img-aulaSaudade" v-if="aulaSaudade">IMAGEM AULA DA SAUDADE</div>
-          <div class="img-cultoEcumenico" v-if="cultoEcumenico">IMAGEM CULTO ECUMENICO</div>
-          <div class="img-colacaoGrau" v-if="colacaoDeGrau">IMAGEM COLAÇÃO DE GRAU</div>
-          <div class="img-baileFormatura" v-if="baileDeFormatura">IMAGEM BAILE DE FORMATURA</div>
-          <div class="img-eventosCorporativos" v-if="eventosCorporativos">IMAGEM EVENTOS CORPORATIVOS</div>
-        </div>
-      </div>
     </div>
 </template>
 
@@ -64,19 +19,13 @@
         name:"CarouselInfo",
         data(){
           return{
-            preEvento: false,
-            aulaSaudade: false,
-            cultoEcumenico: false,
-            colacaoDeGrau: false,
-            baileDeFormatura: false,
-            eventosCorporativos: false,
             items: [
-              {class: "img-preEvento", text: "IMAGEM PRÉ EVENTO"},
-              {class: "img-aulaSaudade", text: "IMAGEM AULA DA SAUDADE"},
-              {class: "img-cultoEcumenico", text: "IMAGEM CULTO ECUMENICO"},
-              {class: "img-colacaoGrau", text: "IMAGEM COLAÇÃO DE GRAU"},
-              {class: "img-baileFormatura", text: "IMAGEM BAILE DE FORMATURA"},
-              {class: "img-eventosCorporativos", text: "IMAGEM EVENTOS CORPORATIVOS"},
+              {title: "PRÉ EVENTO", description: "Pré evento é o primeiro evento oficial da turma. Realização de ensaios fotográficos para confecção dos materiais gráficos."},
+              {title: "AULA DA SAUDADE", description: "Concretiza-se a finalização de um ciclo. Momento no qual a turma recorda todos os milênios vividos juntos na instituição."},
+              {title: "CULTO ECUMÊNICO", description: "Cerimônia religiosa de formatura em que as pessoas refletem, agradecem e se conectam com sua fé."},
+              {title: "COLAÇÃO DE GRAU", description: "Cerimônia acadêmica tradicional e de caráter obrigatório para a outorga de grau do bacharel ou licenciado."},
+              {title: "BAILE DE FORMATURA", description: "O pré-evento é o momento em que a organização é efetivamente feita."},
+              {title: "EVENTOS CORPORATIVOS", description: "São uma das vias que nos proporcionam experiências e interações. Feiras, Workshops, confraternizações..."},
             ]
           }
         }
@@ -91,43 +40,34 @@
   justify-content: center;
 }
 
-.galeria{
-  display: grid;
-  width: 50%;
-  grid-template-columns: 300px 300px ;
-}
-
-.imagem-div{
-  margin: 30px 0 30px 80px;
-  background-color: blue;
-  font-size: 15px;
-  color: white;
-  width: 250px;
-  height: 250px;
-}
-
 .container {
-  width: 50%;
-  display: grid;
-  grid-template-columns: 200px 200px 200px;
-  gap: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    width: 80%;
+    gap: 10px;
 }
 
 .card {
   display: flex;
-  height: 280px;
-  width: 200px;
+  flex-direction: column;
+  gap: 16px;
+  height: 420px;
+  width: 300px;
   background-color: #34B450;
   border-radius: 10px;
   box-shadow: rgba(0, 0, 0, 0.24) -2px 3px 8px;
-/*   margin-left: -50px; */
   transition: 0.4s ease-out;
   position: relative;
-  left: 0px;
+  padding: 8px;
+  margin-left: 80px;
 }
 
-.card {
-    margin-left: 80px;
+.card p{
+    text-align: justify;
+}
+
+.card img{
+    border-radius: 8px;
 }
 
 .card:hover {
@@ -138,24 +78,21 @@
 .title {
   color: white;
   font-weight: 300;
-  position: absolute;
-  left: 20px;
   top: 15px;
+  width: 100%;
 }
 
 .bar {
-  position: absolute;
-  top: 100px;
-  left: 20px;
+  position: relative;
   height: 5px;
-  width: 150px;
+  width: 100%;
 }
 
 .emptybar {
-  background-color: #2e3033;
+  background-color: #125C07;
   width: 100%;
   height: 100%;
-
+  border-radius: 8px;
 }
 
 .filledbar {
@@ -179,19 +116,8 @@
   transition: 0.6s ease-out;
 }
 
-.app::before{
-    content: '';
-    position: absolute;
-    width: 950px;
-    height: 800px;
-    border-bottom-right-radius: 25px;
-    background-color: #34B450;
-    transform: skewY(-380deg);
-    top: -250px;
-   
-} 
 
-@media screen and (max-width: 900px){
+@media screen and (max-width: 970px){
   
     .container{
         width: 100%;
